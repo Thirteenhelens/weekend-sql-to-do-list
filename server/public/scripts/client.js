8c15104cec639655b226a5d36a591d2f255a0768
Jack Barrett <jbarrett1347@gmail.com>
2021-10-15 16:29:28 -0500

    Added get tasks func. on client side

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,15 +3,44 @@ console.log(`HTML loaded`);
 $(document).ready(function () {
     console.log(`jQ loaded`);
 
-    //Click listeners
-    $('#submitButton').on(`click`, submitTask);
-    // $('.deleteButton').on(`click`, xxx, deleteTask);
-    //$('.doneButton').on(`click`, xxx, doneTask);
+    //Call click listener func.
+    clickListeners();
 
+    //Getting all tasks
+    getTasks();
 });
 
-function submitTask() {
-    console.log(`Adding task`);
 
-
+function clickListeners() {
+    // $('#submitButton').on(`click`, submitTask);
+    // $('.deleteButton').on(`click`, xxx, deleteTask);
+    //$('.doneButton').on(`click`, xxx, doneTask);
 }
+
+
+function getTasks() {
+    console.log(`Getting tasks!`);
+
+    $.ajax({
+        method: "GET",
+        url: "/tasks"
+    }).then(function (response) {
+        console.log(`Tasks are:`, response);
+        renderTasks(response);
+    }).catch(function(err) {
+        console.log(err);
+    })
+}
+
+
+// function renderTasks() {
+
+
+// }
+
+
+// function submitTask() {
+//     console.log(`Adding task`);
+
+
+// }
